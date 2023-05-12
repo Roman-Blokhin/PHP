bfc1827fcfcb1444cf7a2df7ca33fca12d21bd6b
Roman <11-roman@mail.ru>
2023-05-12 15:43:02 +0300

    echo + comments

--- lesson_1.php
+++ lesson_1.php
@@ -5,9 +5,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Local server</title>
+    <title>PHP lesson one</title>
 </head>
+
 <body>
-    <h1>Привет, Герой!</h1>
+
+<!-- 1. создаем  код, который позволяет нам выводить текст на экран, можно вписывать html теги -->
+<?php
+    echo '<b>Привет</b>, Герой!';
+    echo '<br/> "Добрый день"';
+
+    // таким образом в php можно прописывать комментарии
+
+    # второй способ писать комментарий
+
+    /*
+      многострочный комментарий
+     */
+
+    echo 'New text';
+
+?>
+
 </body>
 </html>
