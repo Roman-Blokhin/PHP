9399c54e6360914d1f48a818d33cfe88b647af83
Roman <11-roman@mail.ru>
2023-07-04 09:34:51 +0300

    создаем форму обратной связи, добавил новую страницу + страницу для обработки данных + class = "mt-5"

---
+++ feedback.php
@@ -0,0 +1,14 @@
+<?php
+    $title = "Обратная связь";
+    require "blocks/header.php";
+?>
+
+<h1 class="mt-5">Обратная связь</h1> <!-- class="mt-5" от bootstrap дает нам отступ сверху -->
+
+<form action="feedback_post.php" method="post">
+    
+</form>
+
+<?php
+    require "blocks/footer.php";
+?>
