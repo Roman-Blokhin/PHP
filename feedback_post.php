f40bdaff91b524130ffc646c3a429bdb1ae545c0
Roman <11-roman@mail.ru>
2023-07-05 23:28:47 +0300

    запустили сессии на странице, принимающей все данные с формы обратной связи и создали переменные для получения данных

--- feedback_post.php
+++ feedback_post.php
@@ -1,1 +1,8 @@
 <!-- здесь будут обрабатываться данные с формы обратной связи feedback.php -->
+
+<?php
+    session_start(); // запускаем сессии
+    $user_name = htmlspecialchars(trim ($_POST["username"])); // в качестве параметра принимает строку и удаляет из нее все html теги
+    $user_email = htmlspecialchars(trim ($_POST["useremail"]));
+    $user_theme = htmlspecialchars(trim ($_POST["texttheme"]));
+    $texttheme = htmlspecialchars(trim ($_POST["message"])); 
