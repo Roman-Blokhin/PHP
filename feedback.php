9bea9ce4cbd2ce1dc4577eb489c75b79e3f20bf4
Roman <11-roman@mail.ru>
2023-07-06 14:59:35 +0300

    прописали и запустили сессии на странице feedback.php и на странице обработки данных, чтобы данные не пропадали после отправки

--- feedback.php
+++ feedback.php
@@ -1,15 +1,20 @@
 <?php
+    session_start();
     $title = "Обратная связь";
     require "blocks/header.php";
 ?>
 
 <h1 class="mt-5">Обратная связь</h1> <!-- class="mt-5" от bootstrap дает нам отступ сверху -->
 
+<!-- value="" нам нужен для того, чтобы значение переданное в сессии оставалось в поле ввода после обработки данных -->
+
 <form action="feedback_post.php" method="post">
-    <input type="text" name="username" placeholder="Введите имя.." class="form-control">
-    <input type="email" name="useremail" placeholder="Введите почту.." class="form-control">
-    <input type="text" name="texttheme" placeholder="Введите тему сообщения.." class="form-control">
-    <textarea name="message" placeholder="Введите сообщение" class="form-control"></textarea>
+    <input type="text" name="username" value="<?=$_SESSION ["username"]?>" placeholder="Введите имя.." class="form-control">
+    <input type="email" name="useremail" value="<?=$_SESSION ["useremail"]?>" placeholder="Введите почту.." class="form-control">
+    <input type="text" name="texttheme" value="<?=$_SESSION ["texttheme"]?>" placeholder="Введите тему сообщения.." class="form-control">
+    <textarea name="message" placeholder="Введите сообщение" class="form-control"><?=$_SESSION ["message"]?></textarea>
+
+    <!-- в textarea не прописываем value, сессии прописываем между тегами открытия и закрытия -->
 
     <button type="submit" class="btn btn-success">Отправить</button>
 </form>
--- feedback_post.php
+++ feedback_post.php
@@ -5,4 +5,11 @@
     $user_name = htmlspecialchars(trim ($_POST["username"])); // в качестве параметра принимает строку и удаляет из нее все html теги
     $user_email = htmlspecialchars(trim ($_POST["useremail"]));
     $user_theme = htmlspecialchars(trim ($_POST["texttheme"]));
-    $texttheme = htmlspecialchars(trim ($_POST["message"])); 
+    $texttheme = htmlspecialchars(trim ($_POST["message"]));
+
+    // прописываем, что мы обращаемся к сессиям, после обработки данных от пользователя, даннные останутся в форме
+
+    $_SESSION ["username"] = $user_name;
+    $_SESSION ["useremail"] = $user_email;
+    $_SESSION ["texttheme"] = $user_theme;
+    $_SESSION ["message"] = $texttheme;
