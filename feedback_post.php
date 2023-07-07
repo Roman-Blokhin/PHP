c0b4b3b2ef69fff95ea2e9e61ca753b98cd380dd
Roman <11-roman@mail.ru>
2023-07-07 18:03:41 +0300

    написал проверки для полей ввода данных

--- feedback_post.php
+++ feedback_post.php
@@ -5,11 +5,26 @@
     $user_name = htmlspecialchars(trim ($_POST["username"])); // в качестве параметра принимает строку и удаляет из нее все html теги
     $user_email = htmlspecialchars(trim ($_POST["useremail"]));
     $user_theme = htmlspecialchars(trim ($_POST["texttheme"]));
-    $texttheme = htmlspecialchars(trim ($_POST["message"]));
+    $message = htmlspecialchars(trim ($_POST["message"]));
 
     // прописываем, что мы обращаемся к сессиям, после обработки данных от пользователя, даннные останутся в форме
 
     $_SESSION ["username"] = $user_name;
     $_SESSION ["useremail"] = $user_email;
     $_SESSION ["texttheme"] = $user_theme;
-    $_SESSION ["message"] = $texttheme;
+    $_SESSION ["message"] = $message;
+
+    // напишем проверки для полей ввода данных
+
+    if (strlen($user_name) <= 1) {
+        $error_username = "Введите корректное имя";
+    }
+    else if (strlen($user_email) < 5 || strpos($user_email, "@") == false) { // strpos - ищет в переменной конкретный символ
+        $error_email = "Введите корректный email";
+    }
+    else if (strlen($user_theme) < 5) {
+        $error_user_theme = "Тема менее 5 символов";
+    }
+    else if (strlen($message) < 15) {
+        $error_message = "Сообщение менее 15 символов";
+    }
