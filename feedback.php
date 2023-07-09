14ab26755cbff32a91f34c1484b975d372053b3c
Roman <11-roman@mail.ru>
2023-07-09 22:09:34 +0300

    изменили локацию для возврата после отправки формы, создав функцию, использую unset для удаления данных сессий при заходе на страницу, создал сессии, которые выводят ошибку, если поле ввода по условию не заполнено

--- feedback.php
+++ feedback.php
@@ -9,15 +9,21 @@
 <!-- value="" нам нужен для того, чтобы значение переданное в сессии оставалось в поле ввода после обработки данных -->
 
 <form action="feedback_post.php" method="post">
-    <input type="text" name="username" value="<?=$_SESSION ["username"]?>" placeholder="Введите имя.." class="form-control">
-    <div class="text-danger">
-        
-    </div>
-    <input type="email" name="useremail" value="<?=$_SESSION ["useremail"]?>" placeholder="Введите почту.." class="form-control">
-    <input type="text" name="texttheme" value="<?=$_SESSION ["texttheme"]?>" placeholder="Введите тему сообщения.." class="form-control">
-    <textarea name="message" placeholder="Введите сообщение" class="form-control"><?=$_SESSION ["message"]?></textarea>
+    <input type="text" name="username" value="<?=$_SESSION ["user__name"]?>" placeholder="Введите имя.." class="form-control">
+    <div class="text-danger"><?=$_SESSION["error_username"]?></div> <br>
+    <!-- прописываем условия, в которых текст об ошибке будет выводиться сразу в форме обр связи после поля ввода -->
+
+    <input type="email" name="useremail" value="<?=$_SESSION ["usere__mail"]?>" placeholder="Введите почту.." class="form-control">
+    <div class="text-danger"><?=$_SESSION["error_email"]?></div> <br>
+
+    <input type="text" name="texttheme" value="<?=$_SESSION ["text__theme"]?>" placeholder="Введите тему сообщения.." class="form-control">
+    <div class="text-danger"><?=$_SESSION["error_user_theme"]?></div> <br>
+
+    <textarea name="message" placeholder="Введите сообщение" class="form-control"><?=$_SESSION ["message__"]?></textarea>
+    <div class="text-danger"><?=$_SESSION["error_message"]?></div> <br>
 
     <!-- в textarea не прописываем value, сессии прописываем между тегами открытия и закрытия -->
+    <!-- класс от bootstrap, позволяет сделать красный текст -->
 
     <button type="submit" class="btn btn-success">Отправить</button>
 </form>
--- feedback_post.php
+++ feedback_post.php
@@ -2,30 +2,53 @@
 
 <?php
     session_start(); // запускаем сессии
-    $user_name = htmlspecialchars(trim ($_POST["username"])); // в качестве параметра принимает строку и удаляет из нее все html теги
-    $user_email = htmlspecialchars(trim ($_POST["useremail"]));
-    $user_theme = htmlspecialchars(trim ($_POST["texttheme"]));
-    $message = htmlspecialchars(trim ($_POST["message"]));
+
+    unset ($_SESSION ["user__name"]); // при заходе на страницу сессии будут очищаться + очистятся поля ввода
+    unset ($_SESSION ["usere__mail"]);
+    unset ($_SESSION ["text__theme"]);
+    unset ($_SESSION ["message__"]);
+
+    unset ($_SESSION ["error_username"]);
+    unset ($_SESSION ["error_email"]);
+    unset ($_SESSION ["error_user_theme"]);
+    unset ($_SESSION ["error_message"]);
+
+    function redirect() { // прописываем функуию для возврата на страницу обратной связи после обработки данных
+        header('Location: feedback.php');
+        exit;
+    }
+
+    $user_name = htmlspecialchars(trim($_POST["username"])); // в качестве параметра принимает строку и удаляет из нее все html теги
+    $user_email = htmlspecialchars(trim($_POST["useremail"]));
+    $user_theme = htmlspecialchars(trim($_POST["texttheme"]));
+    $message = htmlspecialchars(trim($_POST["message"]));
 
     // прописываем, что мы обращаемся к сессиям, после обработки данных от пользователя, даннные останутся в форме
 
-    $_SESSION ["username"] = $user_name;
-    $_SESSION ["useremail"] = $user_email;
-    $_SESSION ["texttheme"] = $user_theme;
-    $_SESSION ["message"] = $message;
+    $_SESSION ["user__name"] = $user_name;
+    $_SESSION ["usere__mail"] = $user_email;
+    $_SESSION ["text__theme"] = $user_theme;
+    $_SESSION ["message__"] = $message;
 
     // напишем проверки для полей ввода данных
 
     if (strlen($user_name) <= 1) {
-        $_SESSION ["error_username"] = "Введите корректное имя"; /* если возникает ошибка, то мы устанавливаем это значение внутри сессии, 
+        $_SESSION ["error_username"] = "Введите корректное имя"; /* если возникает ошибка, то мы устанавливаем это значение внутри сессии,
         чтобы вывести их в форме обратной связи */
+        redirect(); // вызываем функцию
     }
     else if (strlen($user_email) < 5 || strpos($user_email, "@") == false) { // strpos - ищет в переменной конкретный символ
         $_SESSION ["error_email"] = "Введите корректный email";
+        redirect();
     }
     else if (strlen($user_theme) < 5) {
         $_SESSION ["error_user_theme"] = "Тема менее 5 символов";
+        redirect();
     }
     else if (strlen($message) < 15) {
         $_SESSION ["error_message"] = "Сообщение менее 15 символов";
+        redirect();
+    }
+    else {
+        redirect();
     }
